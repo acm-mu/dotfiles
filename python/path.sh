94a022acde2f0d5567a361059e89b4a8203468f6
Tom Lazar <thomas.lazar@marquette.edu>
2018-10-02 12:06:05 -0500

    Update path.sh

--- python/path.sh
+++ python/path.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# check local bin for python binarys
 test -d $HOME/.local/bin && export PATH=$PATH:$_
 
+# setup pyenv if it exists
 if [ -d $HOME/.pyenv ]; then
     export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
 fi
 
+# if pyenv was setup init it
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
